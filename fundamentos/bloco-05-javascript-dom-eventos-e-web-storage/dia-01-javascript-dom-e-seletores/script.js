6c3540e31bfefe354959a8acbb25061052f97e30
RicardoCaselati <ricardocaselati@gmail.com>
2022-04-13 19:53:59 -0300

    exercício pIII - Adm de Tempo

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -0,0 +1,9 @@
+document.getElementById("header-container").style.backgroundColor = "#2fc18c";
+
+// document.getElementsByClassName('header-container')[0].style.backgroundColor = 'pourple';
+document.getElementsByTagName("div")[0].style.backgroundColor = "pink";
+document.getElementsByTagName("div")[1].style.backgroundColor = "pink";
+
+//document.getElementsByClassName('header-container')[0].style.backgroundColor = 'black';
+document.getElementsByTagName("div")[2].style.backgroundColor = "yellow";
+document.getElementsByTagName("div")[3].style.backgroundColor = "yellow";
