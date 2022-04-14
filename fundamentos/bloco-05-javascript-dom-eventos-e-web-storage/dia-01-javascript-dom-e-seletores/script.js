bb05ccc6d06a14915a9407a520329d5477c9377f
RicardoCaselati <ricardocaselati@gmail.com>
2022-04-14 10:32:07 -0300

    exercícios dia 5.1 1 e 2 - ok

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,11 +1,13 @@
-document.getElementById("header-container").style.backgroundColor = "#2fc18c";
+// Administrador de Tempo da Trybe
+// document.getElementById("header-container").style.backgroundColor = "#2fc18c";
 
-document.getElementsByTagName("h3")[0].style.backgroundColor = '#a500f3';
-document.getElementsByTagName("h3")[1].style.backgroundColor = '#a500f3';
-document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "#ff9f84";
+// document.getElementsByTagName("h3")[0].style.backgroundColor = '#a500f3';
+// document.getElementsByTagName("h3")[1].style.backgroundColor = '#a500f3';
+// document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "#ff9f84";
 
-document.getElementsByTagName("h3")[2].style.backgroundColor = 'black';
-document.getElementsByTagName("h3")[3].style.backgroundColor = 'black';
-document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "#f9db5e";
+// document.getElementsByTagName("h3")[2].style.backgroundColor = 'black';
+// document.getElementsByTagName("h3")[3].style.backgroundColor = 'black';
+// document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "#f9db5e";
 
-document.getElementById("footer-container").style.backgroundColor = "#003533";
+// document.getElementById("footer-container").style.backgroundColor = "#003533";
+
