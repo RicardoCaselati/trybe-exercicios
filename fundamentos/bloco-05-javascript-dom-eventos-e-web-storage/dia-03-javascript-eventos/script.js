44a0ccb8b3ddf7fa780a6756e28acd1406478bbd
RicardoCaselati <ricardocaselati@gmail.com>
2022-04-19 11:57:19 -0300

    exercícios 5.3 - 3 ok

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
@@ -1,33 +1,32 @@
 function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
-  
+
     for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
+        const days = weekDays[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+
+        weekDaysList.appendChild(dayListItem);
     };
-  };
-  
-  createDaysOfTheWeek();
+};
+createDaysOfTheWeek();
 
 
 
-function calendar(){
+function calendar() {
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     const dezDays = document.getElementById("days");
 
-    for (const date of dezDaysList){
+    for (const date of dezDaysList) {
         const dateListItem = document.createElement("li");
         dateListItem.classList.add("day")
 
-        if (date === 24 || date === 25 || date === 31){
+        if (date === 24 || date === 25 || date === 31) {
             dateListItem.classList.add("holiday")
         }
 
-        if (date === 4 || date === 11 || date === 18 || date === 25){
+        if (date === 4 || date === 11 || date === 18 || date === 25) {
             dateListItem.classList.add("friday")
         }
 
@@ -36,17 +35,28 @@ function calendar(){
         dezDays.appendChild(dateListItem);
     }
 }
-
 calendar();
 
 
-function bottomHolidays(param){
-    const buttom = document.createElement("buttom");
-    // _button.data = _a;
-    buttom.innerHTML= "Feriados";
-
-    // _button.onclick = function()
+function buttonHolidays(param) {
+    const button = document.createElement("button");
+    button.innerHTML = "Feriados";
     const divTag = document.getElementsByClassName("buttons-container")[0];
-    divTag.appendChild(buttom)
+
+    // const changeBg = document.getElementsByClassName("holiday")[0];
+    const changeBg = document.querySelectorAll(".holiday");
+
+
+    button.addEventListener("click", colorBg);
+
+    function colorBg() {
+        const changeBg = document.getElementsByClassName("holiday");
+        for (let element of changeBg) {
+            element.classList.toggle("bg-red");
+        }
+    }
+
+    divTag.appendChild(button);
 }
-bottomHolidays("Feriados")
+
+buttonHolidays("Feriados")
